eb759e68c582e6e492549a6a39c289c35bf8bc45
VladimirKardashin <kardv@yandex.ru>
2023-12-31 10:48:23 +0300

    Создание объектов DbServerFactorySqlLite, MsSql

---
+++ SQLAssistant/SQLAssistant22MsSQL/DbServerFactoryMsSql.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanDal
+{
+    public class DbServerFactoryMsSql
+    {
+        public static IDbConnection GetDbConnection()
+        {
+            // MS SQL строка подключения к БД
+            string strCnn = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true";
+            SqlConnection cnn = new SqlConnection(strCnn);
+            return cnn;
+        }
+    }
+}
---
+++ SQLAssistant/SQLAssistant23SqLite/DbServerFactorySqlLite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ScanDal
+{
+    public class DbServerFactorySqlLite
+    {
+        public static IDbConnection GetDbConnection()
+        {
+            // SQLite строка подключения к БД
+            string strCnn = @"Data Source=""C:\Work\C#\WinDbTestApp\SqLite\dbtest.db"";Version=3;";
+            SQLiteConnection cnn = new SQLiteConnection(strCnn);
+            return cnn;
+        }
+    }
+}
